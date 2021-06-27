a12a813d824cb4a5817898ec5d68429717b58974
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-06-27 21:03:52 +0200

    Add material ui view to rss list

--- src/assets/components/RssTable.jsx
+++ src/assets/components/RssTable.jsx
@@ -1,3 +1,7 @@
+import { Table, TableHead, TableRow, TableCell, TableBody, IconButton, TextField } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 import React, { useContext } from 'react';
 import { RssContext } from '../contexts/RssContext';
 
@@ -5,11 +9,44 @@ const RssTable = () => {
   const context = useContext(RssContext);
 
   return (
-    <div>
-      {context.feeds.map(rss => (
-        <div>{rss.title}</div>
-      ))}
-    </div>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Title</TableCell>
+          <TableCell>Url</TableCell>
+          <TableCell>Actions</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell>
+            <TextField/>
+          </TableCell>
+          <TableCell>
+            <TextField/>
+          </TableCell>
+          <TableCell>
+            <IconButton>
+              <AddIcon/>
+            </IconButton>
+          </TableCell>
+        </TableRow>
+        {context.feeds.map(rss => (
+          <TableRow>
+            <TableCell>{rss.title}</TableCell>
+            <TableCell>{rss.url}</TableCell>
+            <TableCell>
+              <IconButton>
+                <EditIcon/>
+              </IconButton>
+              <IconButton>
+                <DeleteIcon/>
+              </IconButton>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
 
