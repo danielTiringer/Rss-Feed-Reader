a63a7db97df7243115a75cfae1932f3a3ca14fc3
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-07-02 22:07:24 +0200

    Implement create functionality

--- src/src/Controller/RssController.php
+++ src/src/Controller/RssController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Rss;
 use App\Repository\RssRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/api/rss', name: 'rss')]
+#[Route('/api/rss', name: 'api_rss')]
 class RssController extends AbstractController
 {
     /**  @var EntityManagerInterface */
@@ -23,7 +26,7 @@ class RssController extends AbstractController
         $this->rssRepository = $rssRepository;
     }
 
-    #[Route('/read', name: 'rss')]
+    #[Route('/read', name: 'api_rss_read')]
     public function index(): Response
     {
         $rss = $this->rssRepository->findAll();
@@ -34,5 +37,26 @@ class RssController extends AbstractController
         }
 
         return $this->json($rssArray);
+    }
+
+    #[Route('/create', name: 'api_rss_create')]
+    public function create(Request $request): Response
+    {
+        $content = json_decode($request->getContent());
+
+        $rss = new Rss();
+        $rss->setTitle($content->title);
+        $rss->setUrl($content->url);
+
+        try {
+           $this->entityManager->persist($rss);
+           $this->entityManager->flush();
+            return $this->json([
+                'rss' => $rss->toArray(),
+            ]);
+        } catch (Exception $exception) {
+
+        }
+
     }
 }
