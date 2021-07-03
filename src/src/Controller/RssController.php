f93d3e41441ceaadd57d4a384e2c22d8d7944b8d
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-07-03 06:44:09 +0200

    Add route methods and modify response type

--- src/src/Controller/RssController.php
+++ src/src/Controller/RssController.php
@@ -7,9 +7,10 @@ use App\Repository\RssRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 #[Route('/api/rss', name: 'api_rss')]
 class RssController extends AbstractController
@@ -26,8 +27,8 @@ class RssController extends AbstractController
         $this->rssRepository = $rssRepository;
     }
 
-    #[Route('/read', name: 'api_rss_read')]
-    public function index(): Response
+    #[Route('/read', name: 'api_rss_read', methods: ['GET'])]
+    public function index(): JsonResponse
     {
         $rss = $this->rssRepository->findAll();
 
@@ -39,8 +40,8 @@ class RssController extends AbstractController
         return $this->json($rssArray);
     }
 
-    #[Route('/create', name: 'api_rss_create')]
-    public function create(Request $request): Response
+    #[Route('/create', name: 'api_rss_create', methods: ['POST'])]
+    public function create(Request $request): JsonResponse
     {
         $content = json_decode($request->getContent());
 
